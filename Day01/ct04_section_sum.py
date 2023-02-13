a7c03a7199a355a0a402f8408e8320cb8b91b80c
mimi9155 <hanbv@naver.com>
2023-02-13 17:30:33 +0900

    2023-02-13 알고리즘 시작

---
+++ Day01/ct04_section_sum.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = tuple(map(int,input().split()))
+numbers = list(map(int,input().split()))
+sums =[0]
+temp = 0
+for i in numbers:
+    temp = temp + i
+    sums.append(temp)
+
+for i in range(M):
+    x, y = map(int, input().split())
+    print(sums[y] - sums[x - 1])
