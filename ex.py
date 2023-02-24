6442436d6337bfcf021437d2d150a755c06e0cf0
mimi9155 <hanbv@naver.com>
2023-02-24 17:19:44 +0900

    2023-02-24 수업종료

--- ex.py
+++ ex.py
@@ -1,11 +1,22 @@
-def solution(my_string, letter):
-    answer = ''
-    a = len(my_string)
-    for i in range(a):
-        if my_string[i] == letter:
-            pass
-        else:
-            answer += my_string[i]
+def solution(n, lost, reserve):
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+
+    for i in reserve_set: # 1,3,5
+
+        if i - 1 in lost_set:
+            lost_set.remove(i - 1)
+
+        elif i + 1 in lost_set:
+            lost_set.remove(i + 1)
+    
+    answer = n - len(lost_set)
     return answer
-            
-print(solution('abcdef','d'))
+
+
+
+n = 5
+lost = [2, 4]
+reserve = [1,3,5]
+
+print(solution(n, lost, reserve))
